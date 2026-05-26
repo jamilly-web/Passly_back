feaf5b5090cf60b977417f159352016b4010fb80
Lucas Henrique <lucashenriquedelimasouza2006@gmail.com>
2026-05-25 21:16:48 -0300

    Feature: Add Operadora Turistica controllers and edit REDME

--- api/src/main/java/dev/Instituicao/Entity/OperadoraTuristicaEntity.java
+++ api/src/main/java/dev/Instituicao/Entity/OperadoraTuristicaEntity.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 
-class OperadoraTuristicaEntity {
+public class OperadoraTuristicaEntity {
 
     private long id;
 
@@ -14,8 +14,49 @@ class OperadoraTuristicaEntity {
     private String Senha;
     private String Login;
     private LocalDate DataCriacao;
-
-    
     private List<LocalTuristicoEntity> PolosVisitacoes;
+
+    public OperadoraTuristicaEntity(
+        String _PnomeUser,
+        String _PNomeInstituicao,
+        String _PCnpj, 
+        String _PSenha, 
+        String _PLogin, 
+        List<LocalTuristicoEntity> _PLocalTuristicoEntities
+    ) {
+        this.NomeUser =_PnomeUser;
+        this.NomeInstituicao = _PNomeInstituicao;
+        this.Cnpj = _PCnpj;
+        this.Senha = _PSenha;
+        this.Login = _PLogin;
+        this.DataCriacao = LocalDate.now();
+        this.PolosVisitacoes = _PLocalTuristicoEntities;
+        
+    }
+    public String getNomeUser() {
+        return NomeUser;
+    }
+    public String getNomeInstituicao() {
+        return NomeInstituicao;
+    }
+    public String getCnpj() {
+        return Cnpj;
+    }
+    public String getSenha() {
+        return Senha;
+    }
+    public String getLogin() {
+        return Login;
+    }
+    public LocalDate getDataCriacao() {
+        return DataCriacao;
+    }
+    public List<LocalTuristicoEntity> getPolosVisitacoes() {
+        return PolosVisitacoes;
+    }
+    public long getId() {
+        return id;
+    }
+
     
 }
